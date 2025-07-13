c69ce8c2ccdc71565073482ca5263f77157cb13f
sarah2008kim <sarah2008kim@gmail.com>
2025-07-13 19:26:32 +0900

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>Smile Unlock</h1>
-      <p>If you're seeing this, deployment worked!</p>
-    </main>
-  );
-}
