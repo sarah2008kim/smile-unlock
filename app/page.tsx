7de6f69c997563b27620d268dbbfc61902c21472
sarah2008kim <sarah2008kim@gmail.com>
2025-07-13 19:46:01 +0900

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Smile Unlock</h1>
+      <p>If you're seeing this, deployment worked!</p>
+    </main>
+  );
+}
