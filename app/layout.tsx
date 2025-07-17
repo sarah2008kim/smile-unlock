245373433ea795ed0b41911fc89193fb6f822e32
sarah2008kim <sarah2008kim@gmail.com>
2025-07-17 10:33:19 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
-// app/layout.tsx
 export const metadata = {
   title: 'Smile Unlock',
   description: 'Unlock with your smile!',
 };
 
-export default function RootLayout({ children }) {
-  children: React.ReactNode;
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
